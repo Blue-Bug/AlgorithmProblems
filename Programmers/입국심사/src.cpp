151337d1955cd88e9994c2aa854d9c5611a6f53c
Blue-Bug <kjj9987@gmail.com>
2021-04-04 03:05:15 +0900

    소스코드

--- Programmers/입국심사/src.cpp
+++ Programmers/입국심사/src.cpp
@@ -6,37 +6,28 @@ using namespace std;
 long long solution(int n, vector<int> times) {
 	long long answer = 0;
 
-	sort(times.begin(), times.end());
-
 	long long people = n;
-
-	if ((people % times.size()) != 0) {
-		people /= times.size();
-		return answer = times.back() * people;
-	}
-	else {
-		people /= times.size();
-		answer = times.back() * (people - 1);
-	}
-	//처음에는 비는대로 막 들어가면 됨
-	//마지막이 문제, 마지막 한명이 어딜 들어가느냐에 따라 종료시간이 갈림
-
-	long long first = 0;
-	long long last = times.size()-1;
-	long long compareTime = times.back();
-	while (first < last) {
-		int mid = (first + last) / 2;
-		//더 기다리지만 심사시간이 더 짧아서 빨리 끝날수 있는가를 체크
-		long long checkTime = times[mid];
-		long long waitTime = checkTime - (answer % checkTime);
-		if (compareTime >= checkTime + waitTime) {
-			last = mid;
-			compareTime = checkTime + waitTime;
+	sort(times.begin(), times.end());
+	long long first = 1;
+	long long last = times.back() * people;
+	answer = last;
+	
+	while (first <= last) {
+		long long mid = (first + last) / 2;
+		long long cnt = 0;
+		for (auto time : times) {
+			cnt += mid / time;
 		}
-		else {
+		if (people > cnt) {
 			first = mid + 1;
 		}
+		else {
+			if (mid < answer) {
+				answer = mid;
+			}
+			last = mid - 1;
+		}
+
 	}
-	answer += compareTime;
 	return answer;
 }
